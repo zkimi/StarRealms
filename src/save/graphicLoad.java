0e42ebbef1eb8c089449cce788eedc119d80a0b1
Zkimi <20417186+zkimi@users.noreply.github.com>
2020-05-16 12:53:28 +0200

    Update graphicLoad.java

--- src/save/graphicLoad.java
+++ src/save/graphicLoad.java
@@ -46,7 +46,6 @@ public class graphicLoad {
 					try {
 						decryptSave();
 					} catch (IOException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				} else {
